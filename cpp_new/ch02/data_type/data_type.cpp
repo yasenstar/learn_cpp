312fa00c5a51549e312cc5dbf0e3ef1f89cad74e
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2023-09-30 15:58:47 -0400

    video 002

---
+++ cpp_new/ch02/data_type/data_type.cpp
@@ -0,0 +1,53 @@
+// data_type.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    // const value
+    //printf("35 + 38 = %d\n", 35 + 48);
+
+    // variable value
+    //int _myclass = 10;
+    //int _myclasS = 16;
+
+    //printf("%d\n", _myclass);
+    //printf("%d\n", _myclasS);
+
+    //std::cout << _myclass;
+    //std::cout << _myclasS;
+
+    // integer data
+
+    /*int abc;
+    abc = 0x12;
+    printf("012 in 10-based number is: %d\n", abc);*/
+
+    // test size in memory
+    /*int a;
+    short b;
+    long c;
+    printf("a, b, c in memory occupy: %d, %d, %d\n", sizeof(a), sizeof(b), sizeof(c));*/
+
+    // effective bits of float and double
+    /*float af = 12.3456783434315454543534534;
+    double ad = 12.5343412354541515154515153;
+    printf("af's effectvie bits is %.30f\n", af);
+    printf("ad's effectvie bits is %.30f\n", ad);*/
+
+    // char
+    printf("abc\ndef\'ghi\"jkl");
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
