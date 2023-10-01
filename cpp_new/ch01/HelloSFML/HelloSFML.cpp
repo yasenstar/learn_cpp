6cb0030dc03d1b3fbdb3fa646308a4c6905a637a
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2023-10-01 12:40:25 -0400

    update

--- cpp_new/ch01/HelloSFML/HelloSFML.cpp
+++ cpp_new/ch01/HelloSFML/HelloSFML.cpp
@@ -1,11 +1,11 @@
-// HelloSFML.cpp : This file contains the 'main' function. Program execution begins and ends there.
+﻿// HelloSFML.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello World! 世界\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
