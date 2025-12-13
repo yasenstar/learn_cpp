3260cd019d0d123bb029f98b98513c06e6aab1e7
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2025-12-13 23:55:45 +0800

    add 7.2.1 & 7.2.2

---
+++ cpp_new/Part2-lang_c/ch07/7.2/7.2_01.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "myfunc.h"
+
+int main() {
+    printHello();
+    printf("a + b = %d\n", addTwoNumber(5,6));
+    return 0;
+}
---
+++ cpp_new/Part2-lang_c/ch07/7.2/myfunc.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void printHello() {
+    printf("hello, how are you?\n");
+}
+
+int addTwoNumber(int a, int b) {
+    return a + b;
+}
