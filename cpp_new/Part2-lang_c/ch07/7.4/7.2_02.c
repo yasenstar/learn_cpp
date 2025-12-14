584c39018d64ef76d4b8481ca7d1886a6173dc0b
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2025-12-14 15:23:09 +0800

    Create 7.2_02.c

---
+++ cpp_new/Part2-lang_c/ch07/7.4/7.2_02.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void changeValue(int b[]) {
+    b[2] = 72;
+    b[4] = 45;
+    // b[7] = 95;
+}
+
+int main() {
+    int a[5];
+    int i;
+    a[0] = 85; a[1] = 70; a[2] = 98; a[3] = 92; a[4] = 78;
+    for (i = 0; i < 5; i++) {
+        printf("a[%d] is %d\t", i, a[i]);
+    }
+    printf("\n");
+    changeValue(a);
+    for (i = 0; i < 5; i++) {
+        printf("a[%d] is %d\t", i, a[i]);
+    }
+    printf("\n");
+}
